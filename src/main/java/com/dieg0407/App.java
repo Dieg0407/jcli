c1c8cffe0929bbd816a363ab405f3a129730e85d
Dieg0407 <diego.pastor.0407@gmail.com>
2025-05-23 15:28:40 -0500

    chore: added picoli and native compile

--- src/main/java/com/dieg0407/App.java
+++ src/main/java/com/dieg0407/App.java
@@ -1,7 +1,21 @@
 package com.dieg0407;
 
-public class App {
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "jcli", mixinStandardHelpOptions = true, version = "1.0.0", description = "A simple Java CLI that emulates net core nuget.")
+public class App implements Callable<Integer> {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Hello World!");
+        return 0;
+    }
+
 }
