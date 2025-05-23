adf4014b0e0c47d9555ef5b5c078a1dc8fb497f2
Dieg0407 <diego.pastor.0407@gmail.com>
2025-05-23 16:04:11 -0500

    chore: removed bin

--- src/main/java/com/dieg0407/App.java
+++ src/main/java/com/dieg0407/App.java
@@ -2,11 +2,16 @@ package com.dieg0407;
 
 import java.util.concurrent.Callable;
 
+import com.dieg0407.commands.NewCommand;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-@Command(name = "jcli", mixinStandardHelpOptions = true, version = "1.0.0", description = "A simple Java CLI that emulates net core nuget.")
+@Command(name = "jcli", mixinStandardHelpOptions = true, version = "1.0.0", description = "A simple Java CLI that emulates net core nuget.", subcommands = {
+        NewCommand.class
+})
 public class App implements Callable<Integer> {
+
     public static void main(String[] args) {
         final int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
---
+++ src/main/java/com/dieg0407/commands/NewCommand.java
@@ -0,0 +1,8 @@
+package com.dieg0407.commands;
+
+import picocli.CommandLine.Command;
+
+@Command(name = "new", description = "Creates a new project.")
+public class NewCommand {
+
+}
