99e7247a9386db8eca63a10d8e34020c75399d72
Dieg0407 <diego.pastor.0407@gmail.com>
2025-05-23 14:55:42 -0500

    chore: removed old code

--- src/main/java/dieg0407/tools/jcli/commands/models/EngineType.java
+++
@@ -1,5 +0,0 @@
-package dieg0407.tools.jcli.commands.models;
-
-public enum EngineType {
-  MAVEN // GRADLE is yet to be supported
-}
--- src/main/java/dieg0407/tools/jcli/services/engines/CommandResult.java
+++
@@ -1,8 +0,0 @@
-package dieg0407.tools.jcli.services.engines;
-
-import java.util.Optional;
-
-public record CommandResult(int exitCode, Optional<String> err, Optional<Exception> exception) {
-
-  public static int SUCCESS = 0;
-}
